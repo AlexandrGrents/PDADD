815ad83b811ac6addeedd5f84bf83c71b9a36b21
Александр Гренц <alx.grents@gmail.com>
2020-07-02 14:25:07 +0500

    Update refinement.cpp

--- PDADD-tornado/refinement.cpp
+++ PDADD-tornado/refinement.cpp
@@ -37,41 +37,6 @@ void findDiscords(float** S, float* C, int* Candidats, float r, int N, int n, in
 	//конец параллельной области
 }
 
-void findDiscords_norm(float *t, float ** u_and_q, float * u_and_q_c, float *C, int *Candidats, float r, int N, int n, 
-	int H, int p, bool **B, int add)
-{
-	//начало параллельной области
-#pragma omp parallel num_threads(p)
-	{
-		
-		int iam = omp_get_thread_num();
-#pragma omp for
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < H; j++)
-			{
-
-				//if (Candidats[j] != (i + add))
-                if (
-                	( (Candidats[j] >= i+add) && ( (Candidats[j] - i+add) > n) )||
-                	( (Candidats[j]  < i+add) && ( (i+add - Candidats[j]) > n) )
-                	)
-				{	
-
-					if (B[iam][j])
-					{
-						B[iam][j] = (ED_norm(t+i, C + j * n, n, u_and_q[0][i], u_and_q_c[j], 
-							u_and_q[1][i], u_and_q_c[j+1]) > r);
-					}
-					else continue;
-				}
-
-			}
-		}
-		
-	}
-	//конец параллельной области
-}
 
 void getDiscords(bool ** B, int *Candidats, int *Discords, int H, int *D, int p)
 {
@@ -97,12 +62,5 @@ void refinement(float **S, bool **B, float *C, int *Candidats, int *Discords, fl
 
 	findDiscords(S, C, Candidats, r, N, n, H, p, B, add);
 
-	getDiscords(B, Candidats, Discords, H, D, p);
-}
-
-void refinement_norm(float *t, float **u_and_q, float * u_and_q_c, bool **B, float *C, int *Candidats, int *Discords, 
-	float r, int N, int n, int H, int *D, int p, int add)
-{
-	findDiscords_norm(t, u_and_q, u_and_q_c, C, Candidats, r, N, n, H, p, B, add);
 	getDiscords(B, Candidats, Discords, H, D, p);
 }
