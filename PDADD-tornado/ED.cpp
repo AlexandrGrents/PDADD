2e88bf8f770b7df121abcc5988651d29c93f9acd
Александр Гренц <alx.grents@gmail.com>
2020-07-02 14:27:08 +0500

    Update ED.cpp

--- PDADD-tornado/ED.cpp
+++ PDADD-tornado/ED.cpp
@@ -19,23 +19,3 @@ double ED_sqr(float *firstSubsequence, float *secondSubsequence, int n)
 	}
 	return ED_sqr;
 }
-double EarlyED_sqr(float *firstSubsequence, float *secondSubsequence, int n, float r)
-{
-	double ED_sqr = 0;
-	for (int i=0; i<n; i++)
-	{
-		ED_sqr +=  pow(firstSubsequence[i] - secondSubsequence[i],2);
-		if (ED_sqr>r) break;
-	}
-	return ED_sqr;
-}
-
-double ED_norm(float *first, float *second, int n, float u1, float u2, float q1, float q2)
-{
-	double q_sum = 0;
-	for (int i =0; i<n; i++)
-	{
-		q_sum += first[i]*second[i];
-	}
-	return 2*n*(1-(q_sum - n*u1*u2)/(n*q1*q2));
-}
